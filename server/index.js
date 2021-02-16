3a32b87ee8d7084c33e552e7729bae13dae6f6be
“lucifer2355” <dgajjar999@gmail.com>
2021-02-16 22:25:51 +0530

    add cors & body-parser middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
 //! MIDDLEWARE
 app.use(express.json());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 module.exports = app;
