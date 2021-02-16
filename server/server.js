14fd345b648549088fb3381365a0a38e4c98981c
“lucifer2355” <dgajjar999@gmail.com>
2021-02-16 21:56:01 +0530

    connect server with database

--- server/server.js
+++ server/server.js
@@ -1,4 +1,26 @@
-const express = require("express");
+/* eslint-disable prettier/prettier */
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./index");
+
+dotenv.config({ path: "../config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successfully");
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`app running on prot ${process.env.PORT}`);
+});
