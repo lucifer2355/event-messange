a87a8c6d0d8afb5479ccc444ea853e5f1faf7d3c
“lucifer2355” <dgajjar999@gmail.com>
2021-03-04 23:12:13 +0530

    import gobal & variables scss file to main scss file

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Container, Typography, Card, CardContent } from "@material-ui/core";
 
 const AuthPage = () => {
-  return <div></div>;
+  return (
+    <div className='container'>
+      <Container maxWidth='md'>
+        <Card className='card'>
+          <CardContent className='card__content'></CardContent>
+        </Card>
+      </Container>
+    </div>
+  );
 };
 
 export default AuthPage;
