477c2cebc09ea565b12590cb17dc8598a6147af7
“lucifer2355” <dgajjar999@gmail.com>
2021-03-04 23:02:53 +0530

    create home & auth page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
+    <div className='container'>
       <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;
