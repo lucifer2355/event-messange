bfd43c0140e8fddaf07a91213218be17dc91da92
“lucifer2355” <dgajjar999@gmail.com>
2021-03-05 22:52:34 +0530

    add tab value, index & tab panel

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -6,37 +6,69 @@ import {
   CardContent,
   Tab,
   Paper,
-  Tabs
+  Tabs,
+  Box
 } from "@material-ui/core";
 
-const AuthPage: React.FC = () => {
-  const [value, setValue] = useState<number>(1);
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
 
-  const handleChange: any = (e: React.FormEvent, value: number) => {
-    e.preventDefault();
-    setValue(value);
+const TabPanel: React.FC<TabPanelProps> = ({
+  children,
+  index,
+  value,
+  ...other
+}) => {
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`auth-form-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+const AuthPage: React.FC = () => {
+  const [value, setValue] = useState<number>(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    event.preventDefault();
+    setValue(newValue);
   };
 
   return (
-    <div className='container'>
-      <Container maxWidth='md'>
-        <Card className='card'>
-          <CardContent className='card__content'>
-            <Paper square>
-              <Tabs
-                value={value}
-                indicatorColor='primary'
-                textColor='primary'
-                onChange={handleChange}
-                aria-label='disabled tabs example'
-              >
-                <Tab label='Login' />
-                <Tab label='Register' />
-              </Tabs>
-            </Paper>
-          </CardContent>
-        </Card>
-      </Container>
+    <div className='auth__form'>
+      <Paper square>
+        <Tabs
+          value={value}
+          indicatorColor='primary'
+          textColor='primary'
+          onChange={handleChange}
+          variant='fullWidth'
+          aria-label='auth form tabs'
+        >
+          <Tab label='Login' />
+          <Tab label='Register' />
+        </Tabs>
+
+        <TabPanel value={value} index={0}>
+          Login
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Register
+        </TabPanel>
+      </Paper>
     </div>
   );
 };
