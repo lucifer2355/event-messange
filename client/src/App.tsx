b8de7fe58bfaf0ac2618ae87c1097a22e3854178
“lucifer2355” <dgajjar999@gmail.com>
2021-03-20 16:15:21 +0530

    add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import AuthPage from "./pages/AuthPage/AuthPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 const App: React.FC = () => {
-  return <AuthPage />;
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' component={HomePage} exact />
+        <Route path='/auth' component={AuthPage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
