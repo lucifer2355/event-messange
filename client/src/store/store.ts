75be4159aeb9a18ae0ccc6d243291d5beaf30c9d
“lucifer2355” <dgajjar999@gmail.com>
2021-03-20 11:50:15 +0530

    add redux persiste

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,7 +1,8 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { persistStore } from "redux-persist";
 
-import combineReducers from "./combineReducers";
+import rootReducer from "./rootReducer";
 
 declare global {
   interface Window {
@@ -11,7 +12,10 @@ declare global {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(
-  combineReducers,
+const store = createStore(
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
+const persistor = persistStore(store);
+
+export default { store, persistor };
