d35211f86c205369fa9314dbe7497424b7bf1eb5
“lucifer2355” <dgajjar999@gmail.com>
2021-03-24 11:25:53 +0530

    add add event state types

--- client/src/store/addEvent/types.tsx
+++ client/src/store/addEvent/types.tsx
@@ -1,5 +1,10 @@
 import { ThunkAction } from "redux-thunk";
 
+export interface EventState {
+  isLoading: boolean;
+  events: object[];
+}
+
 export interface AddEventValues {
   title: string;
   message: string;
@@ -10,3 +15,24 @@ export interface AddEventValues {
   platforms: object[];
   //   media: string;
 }
+
+export const ADD_EVENT_START = "ADD_EVENT_START";
+export const ADD_EVENT_COMPLETE = "ADD_EVENT_COMPLETE";
+
+interface AddEventStartAction {
+  type: typeof ADD_EVENT_START;
+}
+
+interface AddEventCompleteAction {
+  type: typeof ADD_EVENT_COMPLETE;
+  payload: object[];
+}
+
+export type EventActionType = AddEventStartAction | AddEventCompleteAction;
+
+export type AuthDispatch<ReturnType = void> = ThunkAction<
+  ReturnType,
+  EventState,
+  unknown,
+  EventActionType
+>;
