f4a05b7bd57c47683b8866c74ddb9dcee3fc9ccc
“lucifer2355” <dgajjar999@gmail.com>
2021-03-25 22:54:23 +0530

    add create event action

--- client/src/store/addEvent/addEventAction.tsx
+++ client/src/store/addEvent/addEventAction.tsx
@@ -1,3 +1,4 @@
+import axios from "../../api/axios";
 import {
   AddEventValues,
   ADD_EVENT_COMPLETE,
@@ -5,15 +6,30 @@ import {
   EventDispatch,
 } from "./types";
 
+const token = localStorage.getItem("token");
+
 export const createEvent = (
   values: AddEventValues,
   platforms: object[]
 ): EventDispatch => async (dispatch) => {
   dispatch({ type: ADD_EVENT_START });
 
-  console.log("add evnent values", values, platforms);
+  const { data } = await axios.post(
+    "/api/events",
+    {
+      values,
+      platforms,
+    },
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
 
-  //   dispatch({ type: ADD_EVENT_COMPLETE });
+  console.log("response", data);
+
+  dispatch({ type: ADD_EVENT_COMPLETE, payload: data });
   try {
   } catch (error) {
     console.warn("Add Event Error", error);
