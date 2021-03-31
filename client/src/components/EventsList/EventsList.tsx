20b304b5b614869735874607f917140dc19cb4cc
“lucifer2355” <dgajjar999@gmail.com>
2021-03-31 22:43:49 +0530

    add grid in events list component

--- client/src/components/EventsList/EventsList.tsx
+++ client/src/components/EventsList/EventsList.tsx
@@ -1,47 +1,41 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+
 import EventCard from "../EventCard/EventCard";
 
 const EventsList: React.FC = () => {
   const date = new Date().getDate();
   return (
     <div className='event__list'>
-      <div className='event__list__cards-view'>
-        <EventCard
-          title='parth birthday'
-          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
-          dateTime={date}
-          isMail={true}
-          isWhatsApp={true}
-        />
-        <EventCard
-          title='parth birthday'
-          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
-          dateTime={date}
-          isMail={true}
-          isWhatsApp={true}
-        />
-        <EventCard
-          title='parth birthday'
-          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
-          dateTime={date}
-          isMail={true}
-          isWhatsApp={true}
-        />
-        <EventCard
-          title='parth birthday'
-          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
-          dateTime={date}
-          isMail={true}
-          isWhatsApp={true}
-        />
-        <EventCard
-          title='parth birthday'
-          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
-          dateTime={date}
-          isMail={true}
-          isWhatsApp={true}
-        />
-      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={4} sm={12} md={6}>
+          <EventCard
+            title='parth birthday'
+            message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
+            dateTime={date}
+            isMail={true}
+            isWhatsApp={true}
+          />
+        </Grid>
+        <Grid item xs={4} sm={12} md={6}>
+          <EventCard
+            title='parth birthday'
+            message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
+            dateTime={date}
+            isMail={true}
+            isWhatsApp={true}
+          />
+        </Grid>
+        <Grid item xs={4} sm={12} md={6}>
+          <EventCard
+            title='parth birthday'
+            message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
+            dateTime={date}
+            isMail={true}
+            isWhatsApp={true}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 };
