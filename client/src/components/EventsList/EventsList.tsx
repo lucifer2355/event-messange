7183734c4612c01ce95e15ef2be921b41a5cd64a
“lucifer2355” <dgajjar999@gmail.com>
2021-03-31 22:32:01 +0530

    add some events list style

--- client/src/components/EventsList/EventsList.tsx
+++ client/src/components/EventsList/EventsList.tsx
@@ -4,14 +4,44 @@ import EventCard from "../EventCard/EventCard";
 const EventsList: React.FC = () => {
   const date = new Date().getDate();
   return (
-    <div>
-      <EventCard
-        title='parth birthday'
-        message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
-        dateTime={date}
-        isMail={true}
-        isWhatsApp={true}
-      />
+    <div className='event__list'>
+      <div className='event__list__cards-view'>
+        <EventCard
+          title='parth birthday'
+          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
+          dateTime={date}
+          isMail={true}
+          isWhatsApp={true}
+        />
+        <EventCard
+          title='parth birthday'
+          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
+          dateTime={date}
+          isMail={true}
+          isWhatsApp={true}
+        />
+        <EventCard
+          title='parth birthday'
+          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
+          dateTime={date}
+          isMail={true}
+          isWhatsApp={true}
+        />
+        <EventCard
+          title='parth birthday'
+          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
+          dateTime={date}
+          isMail={true}
+          isWhatsApp={true}
+        />
+        <EventCard
+          title='parth birthday'
+          message='Happy birthday to my favorite former punk rocker!” (Or: hippie, goth, Valley Girl, grunge rocker)'
+          dateTime={date}
+          isMail={true}
+          isWhatsApp={true}
+        />
+      </div>
     </div>
   );
 };
