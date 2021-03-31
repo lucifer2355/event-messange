a3daa96274a2764537ffdaccee7ccb3d10b2c62d
“lucifer2355” <dgajjar999@gmail.com>
2021-03-31 23:04:21 +0530

    add get events store type

--- client/src/store/getEvents/types.tsx
+++ client/src/store/getEvents/types.tsx
@@ -1,1 +1,24 @@
 import { ThunkAction } from "redux-thunk";
+
+import { EventState } from "../../../types";
+
+export const GET_EVENTS_START = "GET_EVENTS_START";
+export const GET_EVENTS_COMPLETE = "GET_EVENT_COMPLETE";
+
+interface GetEventsStartAction {
+  type: typeof GET_EVENTS_START;
+}
+
+interface GetEventsCompleteAction {
+  type: typeof GET_EVENTS_COMPLETE;
+  payload: object[];
+}
+
+export type GetEventActionType = GetEventsStartAction | GetEventsCompleteAction;
+
+export type EventDispatch<ReturnType = void> = ThunkAction<
+  ReturnType,
+  EventState,
+  unknown,
+  EventActionType
+>;
