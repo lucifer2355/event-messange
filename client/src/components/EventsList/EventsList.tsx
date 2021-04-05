0329d0922c053850d2c43f6db80e0a09c94f179e
“lucifer2355” <dgajjar999@gmail.com>
2021-04-05 23:21:26 +0530

    diaplay user created events

--- client/src/components/EventsList/EventsList.tsx
+++ client/src/components/EventsList/EventsList.tsx
@@ -4,14 +4,18 @@ import { Grid } from "@material-ui/core";
 import EventCard from "../EventCard/EventCard";
 import { AddEventValues } from "../../store/addEvent/types";
 
-const EventsList: React.FC<AddEventValues> = ({data}) => {
+interface EventsListProps {
+  data: any;
+}
+
+const EventsList: React.FC<EventsListProps> = ({ data }) => {
   console.log(data);
 
   return (
     <div className='event__list'>
       <Grid container spacing={2}>
-        {data.data.length > 0 &&
-          data.map((event: AddEventValues) => (
+        {data.length > 0 &&
+          data.map((event: any) => (
             <Grid item xs={4} sm={12} md={6}>
               <EventCard
                 title={event.title}
