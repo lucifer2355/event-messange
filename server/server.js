ce6daf2aa2716ff6250ae39118cfdad6abad30a5
“lucifer2355” <dgajjar999@gmail.com>
2021-04-06 23:31:23 +0530

    add twilio for whatsApp messages

--- server/server.js
+++ server/server.js
@@ -5,6 +5,20 @@ const app = require("./index");
 
 dotenv.config({ path: "../config.env" });
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_TOKEN;
+
+// eslint-disable-next-line import/order
+const client = require("twilio")(accountSid, authToken);
+
+client.messages
+  .create({
+    from: "whatsapp:+919512531333",
+    body: "This message sent with help of code so ignore it and do some work.",
+    to: "whatsapp:+917878111271",
+  })
+  .then((message) => console.log(message.sid));
+
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
