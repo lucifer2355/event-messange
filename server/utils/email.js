2bea5f9f69b0a2dfa94ae7125d87c93dafd71465
“lucifer2355” <dgajjar999@gmail.com>
2021-04-10 23:16:11 +0530

    add mail file

---
+++ server/utils/email.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  //! Create transporter
+  //   const transpoter = nodemailer.createTransport({
+  //     service: "Gmail",
+  //     auth: {
+  //       user: process.env.EMAIL_USERNAME,
+  //       pass: process.env.EMAIL_PASSWORD,
+  //     },
+  //*     Activate in gmail 'less secure app' option
+  //   });
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  //! Define the email options
+  const mailOptions = {
+    from: "Dhruvil Gajjar <dgajjar000@gmail.com>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  //! Actually send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
