7579a0b51cfb44a192e3dde1b8222b988a4fb611
“lucifer2355” <dgajjar999@gmail.com>
2021-04-13 22:24:26 +0530

    add the email template pug file

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,11 +1,52 @@
 const nodemailer = require("nodemailer");
+const pug = require("pug");
+const htmlToText = require("html-to-text");
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(" ")[0];
     this.url = url;
-    this.from = "Dhruvil Gajjar <dgajjar000@gmail.com>";
+    this.from = `Dhruvil Gajjar <${process.env.EMAIL_FROM}>`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === "production") {
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  send(template, subject) {
+    //! Send the actual email
+    const html = pug.renderFile(`${__dirname}/../../views/${template}.pug`, {
+      filename: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    //! Define email option
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    //! Create transport and send email
+  }
+
+  sendWelcome() {
+    this.send("welcome", "Welcome to the family! 😊");
   }
 };
 
@@ -20,26 +61,8 @@ const sendEmail = async (options) => {
   //*     Activate in gmail 'less secure app' option
   //   });
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
   //! Define the email options
-  const mailOptions = {
-    from: "Dhruvil Gajjar <dgajjar000@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
 
   //! Actually send the email
   await transporter.sendMail(mailOptions);
 };
-
-module.exports = sendEmail;
