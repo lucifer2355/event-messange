8bf846460184c5e87b9ed1acf882634e942d8643
“lucifer2355” <dgajjar999@gmail.com>
2021-05-14 12:34:06 +0530

    remove access header

--- server/index.js
+++ server/index.js
@@ -13,15 +13,6 @@ const eventRouter = require("./routes/eventRoutes");
 const app = express();
 
 //! MIDDLEWARE
-app.use((_req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-});
 app.use(cors());
 app.use(express.json());
 //* Body parser, reading data from body into req.body
